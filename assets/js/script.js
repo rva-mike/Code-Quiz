b549c5296bca4cd59cfc67f73fd8a3a1f9a2b879
Mike <mike.levy.rva@gmail.com>
2022-06-19 16:07:16 -0400

    added questions var in js

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: [
+            { text: "javascript", correct: false },
+            { text: "script", correct: true },
+            { text: "js", correct: false },
+            { text: "java", correct: false }
+        ]
+    },
+    {
+        question: "Where is the correct place to insert JavaScript?",
+        answers: [
+            { text: "The Head Section", correct: false },
+            { text: "The Body Section", correct: false },
+            { text: "In an External File", correct: false },
+            { text: "All of the Above", correct: true }
+        ]
+    },
+]
