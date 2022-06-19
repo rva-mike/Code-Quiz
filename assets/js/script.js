a904e64233a0be3568779da3e435f826a1a96729
Mike <mike.levy.rva@gmail.com>
2022-06-19 16:43:44 -0400

    added/created question & answer related functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,11 +50,37 @@ function startGame() {
 
 
 function showQuestion(question) {
-    questionEl.innerText = question.question; //question.question
+    questionEl.innerText = question.question;
     question.answers.forEach(answer => {
         var button = document.createElement("button");
         button.innerText = answer.text;
         button.classList.add("btn");
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
         answerButtonsEl.appendChild(button);
-      });
+    });
+}
+
+function selectAnswer(event) {
+    var selectedButton = event.target;
+    if (!selectedButton.dataset.correct) {
+        timer = timer - 10;
+        console.log(timer);
+    }
+    if (questionNumber == questions.length - 1) {
+        gameOver();
+    } else {
+        clearQuestion();
+        questionNumber++;
+        showQuestion(questions[questionNumber]);
+        console.log(score);
+    }
+}
+
+function clearQuestion() {
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild);
+    }
 }
